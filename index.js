4d2914689d9ea16d1e5d132f7870d8f253288487
Derrick <similienderrick@yahoo.com>
2025-01-05 15:47:48 -0500

    Initialized git repository

--- index.js
+++ index.js
@@ -4,22 +4,34 @@
 
 function contact() {
     event.preventDefault();
-    // emailjs
-    // .sendForm(
-    //     'service_shu41ah',
-    //     'template_whckks8',
-    //     event.target,
-    //      'yymNb5FJCpQNrIp8E'
-    // ).then(() => {
-    //   console.log('this worked1')  
-    // })
     const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--sucess')
-    loading.classList += " modal__overlay--visible";
-    setTimeout(() => {
+    const success = document.querySelector('.modal__overlay--success')
+    success.classList += " modal__overlay--visible";
+
+    emailjs
+    .sendForm(
+        'service_shu41ah',
+        'template_whckks8',
+        event.target,
+         'yymNb5FJCpQNrIp8E'
+    ).then(() => {
+        // throw new Error("error");
         loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-        console.log('it worked 1')
-    }, 1000);
+        success.classList += " modal__overlay--visible"; 
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at email@email.com"
+        );
+    })
+
+    // // const loading = document.querySelector('.modal__overlay--loading')
+    // // const success = document.querySelector('.modal__overlay--sucess')
+    // loading.classList += " modal__overlay--visible";
+    // setTimeout(() => {
+    //     // loading.classList.remove("modal__overlay--visible");
+    //     // success.classList += " modal__overlay--visible";
+    //     console.log('it worked 1')
+    // }, 1000);
 
 }
